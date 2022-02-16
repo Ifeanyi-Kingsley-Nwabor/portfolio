03ebbf1722971021a2d1c7f5281e8c293f74588c
Codify KING <81686044+Ifeanyi-Kingsley-Nwabor@users.noreply.github.com>
2022-02-16 17:19:29 +0100

    edit text in  project map

--- src/components/ProjectMap.js
+++ src/components/ProjectMap.js
@@ -6,9 +6,9 @@ const ProjectMap = () => {
   // console.log(ProjectData);
   return (
     <div className="container">
-      <h5 className="project-map">
+      <p className="project-map">
         Here are a few Projects I participated in during the Full-Stack Web and
-        App Development Training. I would also be adding some{" "}
+        App Development Training. I would also be adding more{" "}
         <a
           href="https://aws.amazon.com"
           target="_blank"
@@ -16,8 +16,12 @@ const ProjectMap = () => {
         >
           AWS
         </a>{" "}
-        cloud solution project soon.
-      </h5>
+        cloud solution project soon. I had to take down the Zombeatz Amplify App
+        as this was built using the free tier AWS resources. Since my 12 months
+        Grace period has ran out, I intend to recreate this application and some
+        others in a new AWS account. This would eventually take a couple of days
+        so I'll really appreciate your understanding.
+      </p>
 
       {ProjectData &&
         ProjectData.map((oneProject, index) => (
